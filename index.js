a3a57b9d893f6f9dafbc2a8208940cb4bc87870a
Saloni Sharma <salonisharma9700@gmail.com>
2024-07-06 05:04:42 +0530

    1.0

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const cors = require('cors');
+
 const path = require('path');
 require('dotenv').config(); 
 
@@ -9,6 +9,8 @@ const uploadRoutes = require('./routes/api');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const cors = require('cors');
+app.use(cors());
 
 // app.use(cors({
 //     // origin: ['http://localhost:3000', 'http://localhost:3001',
@@ -19,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 //     credentials: true,
 // }));
 
-app.use(cors());
+
 
 app.use(express.json());
 app.use(bodyParser.json());
