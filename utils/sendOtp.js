5cf585de6891ca4cc7afc194a29a594d6c42767c
Saloni Sharma <140889549+salonisharma9700@users.noreply.github.com>
2024-07-05 15:19:24 +0530

    1.0

--- utils/sendOtp.js
+++ utils/sendOtp.js
@@ -1,33 +1,4 @@
-// // utils/sendOtp.js
-// const twilio = require('twilio');
 
-// // Twilio credentials
-// const accountSid = 'AC6322364174f8e19755ff9fcbee4d8470';
-// const authToken = '3927786dfa370344bfd1d5984baff3eb';
-// const twilioPhoneNumber = '+17815635972';
-
-// const client = twilio(accountSid, authToken);
-
-// const sendOtp = async (contactNumber, otp) => {
-//   try {
-//     // Prepend +91 if not already present
-//     if (!contactNumber.startsWith('+')) {
-//       contactNumber = `+91${contactNumber}`;
-//     }
-
-//     await client.messages.create({
-//       body: `Your OTP code is ${otp}`,
-//       from: twilioPhoneNumber,
-//       to: contactNumber,
-//     });
-//     console.log(`Sending OTP ${otp} to ${contactNumber}`);
-//   } catch (error) {
-//     console.error('Error sending OTP:', error.message);
-//     throw new Error('Failed to send OTP');
-//   }
-// };
-
-// module.exports = sendOtp;
 
 
 const twilio = require('twilio');
